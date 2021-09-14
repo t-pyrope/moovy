db0d158d8252c58cfea12e4674aa0e1474d6eab8
badylbek <Begaiym.Adylbek.Kyzy@teskalabs.com>
2021-09-14 08:51:32 +0200

    Slashes and the end of url

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const baseUrl = `http://www.omdbapi.com/`
  
-export const titleSearchURL = (title, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&s=${title}&page=${page}`
+export const titleSearchURL = (title, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&s=${title}&page=${page}/`
 
-export const yearSearchURL = (year, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&y=${year}&page=${page}`;
+export const yearSearchURL = (year, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&y=${year}&page=${page}/`;
 
-export const titleYearSearchURL = (title, year, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&s=${title}&y=${year}&page=${page}`;
+export const titleYearSearchURL = (title, year, page = 1) => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&s=${title}&y=${year}&page=${page}/`;
 
-export const movieDetailURL = id => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&i=${id}`;
+export const movieDetailURL = id => `${baseUrl}?apikey=${process.env.REACT_APP_API_KEY}&i=${id}/`;

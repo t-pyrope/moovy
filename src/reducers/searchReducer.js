5e9d55637bf56644c6c48c40cde77e709f2dac22
badylbek <Begaiym.Adylbek.Kyzy@teskalabs.com>
2021-09-21 15:13:12 +0200

    Remove repetitions in code

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -5,41 +5,32 @@ const initState = {
     page: 1,
     searchBy: '',
     length: 0,
-    initial: true,
+    errorMessage: ''
 };
 
 const searchReducer = (state=initState, action) => {
+    let searchedMovies = action.payload?.searchedMovies ?? [];
+    let title = action.payload?.title;
+    let page = action.payload?.page;
+    let searchBy = action.payload?.searchBy;
+    let length = action.payload?.length;
     switch (action.type) {
         case 'FETCH_SEARCH_TITLE':
             return {
                 ...state,
-                searchedMovies: action.payload.searchedMovies,
-                title: action.payload.title,
-                page: action.payload.page,
-                searchBy: action.payload.searchBy,
-                length: action.payload.length,
-                initial: false,
-            };
-        case 'FETCH_SEARCH_YEAR':
-            return {
-                ...state,
-                searchedMovies: action.payload.searchedMovies,
-                year: action.payload.year,
-                page: action.payload.page,
-                searchBy: action.payload.searchBy,
-                length: action.payload.length,
-                initial: false,
+                searchedMovies, title, page, searchBy, length,
+                errorMessage: !searchedMovies.length
+                    ? `Couldn't find anything for ${action.payload.title}`
+                    : '',
             };
         case 'FETCH_SEARCH_BOTH':
+            const year = action.payload.year;
             return {
                 ...state,
-                searchedMovies: action.payload.searchedMovies,
-                title: action.payload.title,
-                year: action.payload.year,
-                page: action.payload.page,
-                searchBy: action.payload.searchBy,
-                length: action.payload.length,
-                initial: false,
+                searchedMovies, title, page, searchBy, length, year,
+                errorMessage: !searchedMovies.length
+                    ? `Couldn't find anything for ${title}, ${year}`
+                    : '',
             };
         default:
             return { ...state };
